3c043550de50d6522580cd9f3245332fdf1ea7a6
allu16 <62132801+allu16@users.noreply.github.com>
2025-05-06 17:24:37 +0300

    uusi projekti joka ei käytä ef corea ja pomeloa

---
+++ MokkivarausApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MokkivarausApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}

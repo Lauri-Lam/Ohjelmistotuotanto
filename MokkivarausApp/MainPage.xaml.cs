fa0fc84856ab18c0d35766ed5f21047959aee79b
Lauri-Lam <laurilam360@gmail.com>
2025-11-13 02:10:06 +0200

    GOAT of maui signing out

--- MokkivarausApp/MainPage.xaml.cs
+++ MokkivarausApp/MainPage.xaml.cs
@@ -2,36 +2,8 @@
 
 public partial class MainPage : ContentPage
 {
-<<<<<<< Updated upstream
-    using MokkivarausApp.Data;
-    using System;
-    using System.Data;
-    using System.Diagnostics;
-    using System.Threading.Tasks;
-
-    public partial class MainPage : ContentPage
-    {
-        DatabaseService dbS = new DatabaseService();
-
-        public MainPage()
-        {
-            InitializeComponent();
-            LoadData();
-        }
-
-        private async void LoadData()
-        {
-            DataTable dt = await dbS.GetDataAsync("SELECT * FROM alue");
-
-            foreach (DataRow row in dt.Rows)
-            {
-                Trace.WriteLine(row["nimi"]);
-            }
-        }
-=======
     public MainPage()
     {
         InitializeComponent();
->>>>>>> Stashed changes
     }
 }
